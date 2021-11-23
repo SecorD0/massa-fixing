adrien-zinger/massa

Use a &'static ref of settings rather than cloning configuration at runtime across modules

api/src/lib.rs
@@ -11,7 +11,7 @@ use jsonrpc_derive::rpc;
 use jsonrpc_http_server::{CloseHandle, ServerBuilder};
 use models::address::{AddressHashMap, AddressHashSet};
 use models::api::{
-    APIConfig, AddressInfo, BlockInfo, BlockSummary, EndorsementInfo, NodeStatus, OperationInfo,
+    APISettings, AddressInfo, BlockInfo, BlockSummary, EndorsementInfo, NodeStatus, OperationInfo,
     TimeInterval,
 };
 use models::clique::Clique;
@@ -19,7 +19,7 @@ use models::crypto::PubkeySig;
 use models::node::NodeId;
 use models::operation::{Operation, OperationId};
 use models::{Address, BlockId, EndorsementId, Version};
-use network::{NetworkCommandSender, NetworkConfig};
+use network::{NetworkCommandSender, NetworkSettings};
 use pool::PoolCommandSender;
 use signature::PrivateKey;
 use std::net::{IpAddr, SocketAddr};
@@ -35,9 +35,9 @@ mod public;
 pub struct Public {
     pub consensus_command_sender: ConsensusCommandSender,
     pub pool_command_sender: PoolCommandSender,
-    pub consensus_config: ConsensusConfig,
-    pub api_config: APIConfig,
-    pub network_config: NetworkConfig,
+    pub consensus_settings: &'static ConsensusConfig,
+    pub api_settings: &'static APISettings,
+    pub network_settings: &'static NetworkSettings,
     pub version: Version,
     pub network_command_sender: NetworkCommandSender,
     pub compensation_millis: i64,
@@ -47,8 +47,8 @@ pub struct Public {
 pub struct Private {
     pub consensus_command_sender: ConsensusCommandSender,
     pub network_command_sender: NetworkCommandSender,
-    pub consensus_config: ConsensusConfig,
-    pub api_config: APIConfig,
+    pub consensus_settings: &'static ConsensusConfig,
+    pub api_settings: &'static APISettings,
     pub stop_node_channel: mpsc::Sender<()>,
 }
 

api/src/public.rs
@@ -9,8 +9,8 @@ use futures::{stream::FuturesUnordered, StreamExt};
 use jsonrpc_core::BoxFuture;
 use models::address::{AddressHashMap, AddressHashSet};
 use models::api::{
-    APIConfig, AddressInfo, BlockInfo, BlockInfoContent, BlockSummary, EndorsementInfo, NodeStatus,
-    OperationInfo, TimeInterval,
+    APISettings, AddressInfo, BlockInfo, BlockInfoContent, BlockSummary, EndorsementInfo,
+    NodeStatus, OperationInfo, TimeInterval,
 };
 use models::clique::Clique;
 use models::crypto::PubkeySig;
@@ -21,7 +21,7 @@ use models::{
     Address, BlockHashSet, BlockId, EndorsementId, Operation, OperationHashMap, OperationHashSet,
     OperationId, Slot, Version,
 };
-use network::{NetworkCommandSender, NetworkConfig};
+use network::{NetworkCommandSender, NetworkSettings};
 use pool::PoolCommandSender;
 use signature::PrivateKey;
 use std::net::{IpAddr, SocketAddr};
@@ -30,10 +30,10 @@ use time::UTime;
 impl API<Public> {
     pub fn new(
         consensus_command_sender: ConsensusCommandSender,
-        api_config: APIConfig,
-        consensus_config: ConsensusConfig,
+        api_settings: &'static APISettings,
+        consensus_settings: &'static ConsensusConfig,
         pool_command_sender: PoolCommandSender,
-        network_config: NetworkConfig,
+        network_settings: &'static NetworkSettings,
         version: Version,
         network_command_sender: NetworkCommandSender,
         compensation_millis: i64,
@@ -41,10 +41,10 @@ impl API<Public> {
     ) -> Self {
         API(Public {
             consensus_command_sender,
-            consensus_config,
-            api_config,
+            consensus_settings,
+            api_settings,
             pool_command_sender,
-            network_config,
+            network_settings,
             version,
             network_command_sender,
             compensation_millis,
@@ -92,19 +92,19 @@ impl Endpoints for API<Public> {
     fn get_status(&self) -> BoxFuture<Result<NodeStatus, ApiError>> {
         let consensus_command_sender = self.0.consensus_command_sender.clone();
         let network_command_sender = self.0.network_command_sender.clone();
-        let network_config = self.0.network_config.clone();
+        let network_config = self.0.network_settings.clone();
         let version = self.0.version;
-        let consensus_config = self.0.consensus_config.clone();
+        let consensus_settings = self.0.consensus_settings.clone();
         let compensation_millis = self.0.compensation_millis;
         let mut pool_command_sender = self.0.pool_command_sender.clone();
         let node_id = self.0.node_id;
-        let algo_config = consensus_config.to_algo_config();
+        let algo_config = consensus_settings.to_algo_config();
         let closure = async move || {
             let now = UTime::now(compensation_millis)?;
             let last_slot = get_latest_block_slot_at_timestamp(
-                consensus_config.thread_count,
-                consensus_config.t0,
-                consensus_config.genesis_timestamp,
+                consensus_settings.thread_count,
+                consensus_settings.t0,
+                consensus_settings.genesis_timestamp,
                 now,
             )?;
 
@@ -118,11 +118,11 @@ impl Endpoints for API<Public> {
                 node_id,
                 node_ip: network_config.routable_ip,
                 version,
-                genesis_timestamp: consensus_config.genesis_timestamp,
-                t0: consensus_config.t0,
-                delta_f0: consensus_config.delta_f0,
-                roll_price: consensus_config.roll_price,
-                thread_count: consensus_config.thread_count,
+                genesis_timestamp: consensus_settings.genesis_timestamp,
+                t0: consensus_settings.t0,
+                delta_f0: consensus_settings.delta_f0,
+                roll_price: consensus_settings.roll_price,
+                thread_count: consensus_settings.thread_count,
                 current_time: now,
                 connected_nodes: peers?
                     .peers
@@ -133,7 +133,7 @@ impl Endpoints for API<Public> {
                 last_slot,
                 next_slot: last_slot
                     .unwrap_or_else(|| Slot::new(0, 0))
-                    .get_next_slot(consensus_config.thread_count)?,
+                    .get_next_slot(consensus_settings.thread_count)?,
                 consensus_stats: consensus_stats?,
                 network_stats: network_stats?,
                 pool_stats: pool_stats?,
@@ -141,7 +141,7 @@ impl Endpoints for API<Public> {
                 algo_config,
                 current_cycle: last_slot
                     .unwrap_or_else(|| Slot::new(0, 0))
-                    .get_cycle(consensus_config.periods_per_cycle),
+                    .get_cycle(consensus_settings.periods_per_cycle),
             })
         };
         Box::pin(closure())
@@ -163,7 +163,7 @@ impl Endpoints for API<Public> {
         &self,
         ops: Vec<OperationId>,
     ) -> BoxFuture<Result<Vec<OperationInfo>, ApiError>> {
-        let api_cfg = self.0.api_config;
+        let api_cfg = self.0.api_settings;
         let consensus_command_sender = self.0.consensus_command_sender.clone();
         let mut pool_command_sender = self.0.pool_command_sender.clone();
         let closure = async move || {
@@ -277,13 +277,13 @@ impl Endpoints for API<Public> {
         time: TimeInterval,
     ) -> BoxFuture<Result<Vec<BlockSummary>, ApiError>> {
         let consensus_command_sender = self.0.consensus_command_sender.clone();
-        let consensus_config = self.0.consensus_config.clone();
+        let consensus_settings = self.0.consensus_settings.clone();
         let closure = async move || {
             // filter blocks from graph_export
             let (start_slot, end_slot) = time_range_to_slot_range(
-                consensus_config.thread_count,
-                consensus_config.t0,
-                consensus_config.genesis_timestamp,
+                consensus_settings.thread_count,
+                consensus_settings.t0,
+                consensus_settings.genesis_timestamp,
                 time.start,
                 time.end,
             )?;
@@ -330,8 +330,8 @@ impl Endpoints for API<Public> {
         addresses: Vec<Address>,
     ) -> BoxFuture<Result<Vec<AddressInfo>, ApiError>> {
         let cmd_sender = self.0.consensus_command_sender.clone();
-        let cfg = self.0.consensus_config.clone();
-        let api_cfg = self.0.api_config;
+        let cfg = self.0.consensus_settings.clone();
+        let api_cfg = self.0.api_settings;
         let pool_command_sender = self.0.pool_command_sender.clone();
         let compensation_millis = self.0.compensation_millis;
         let closure = async move || {
@@ -434,7 +434,7 @@ impl Endpoints for API<Public> {
         ops: Vec<Operation>,
     ) -> BoxFuture<Result<Vec<OperationId>, ApiError>> {
         let mut cmd_sender = self.0.pool_command_sender.clone();
-        let api_cfg = self.0.api_config;
+        let api_cfg = self.0.api_settings;
         let closure = async move || {
             if ops.len() as u64 > api_cfg.max_arguments {
                 return Err(ApiError::TooManyArguments("too many arguments".into()));

consensus/src/consensus_controller.rs
@@ -1,12 +1,15 @@
 // Copyright (c) 2021 MASSA LABS <info@massa.net>
 
-use std::{collections::VecDeque, path::Path};
-
-use tokio::{
-    sync::{mpsc, oneshot},
-    task::JoinHandle,
+use super::{
+    block_graph::*,
+    consensus_worker::{
+        ConsensusCommand, ConsensusEvent, ConsensusManagementCommand, ConsensusWorker,
+    },
+    pos::ProofOfStake,
+    settings::{ConsensusConfig, CHANNEL_SIZE},
 };
-
+use crate::error::ConsensusError;
+use crate::pos::ExportProofOfStake;
 use models::{
     address::{AddressHashMap, AddressHashSet, AddressState},
     BlockHashMap, OperationHashMap, OperationHashSet,
@@ -16,17 +19,10 @@ use models::{Address, Block, BlockId, OperationSearchResult, Slot, StakersCycleP
 use pool::PoolCommandSender;
 use protocol_exports::{ProtocolCommandSender, ProtocolEventReceiver};
 use signature::{derive_public_key, PrivateKey, PublicKey};
-
-use crate::error::ConsensusError;
-use crate::pos::ExportProofOfStake;
-
-use super::{
-    block_graph::*,
-    config::{ConsensusConfig, CHANNEL_SIZE},
-    consensus_worker::{
-        ConsensusCommand, ConsensusEvent, ConsensusManagementCommand, ConsensusWorker,
-    },
-    pos::ProofOfStake,
+use std::{collections::VecDeque, path::Path};
+use tokio::{
+    sync::{mpsc, oneshot},
+    task::JoinHandle,
 };
 use tracing::{debug, error, info};
 
@@ -37,7 +33,7 @@ use tracing::{debug, error, info};
 /// * protocol_command_sender: a ProtocolCommandSender instance to send commands to Protocol.
 /// * protocol_event_receiver: a ProtocolEventReceiver instance to receive events from Protocol.
 pub async fn start_consensus_controller(
-    cfg: ConsensusConfig,
+    cfg: ConsensusConfig, // TODO: should be a &'static ConsensusSettings
     protocol_command_sender: ProtocolCommandSender,
     protocol_event_receiver: ProtocolEventReceiver,
     pool_command_sender: PoolCommandSender,

massa-node/src/settings.rs
@@ -1,20 +1,23 @@
 // Copyright (c) 2021 MASSA LABS <info@massa.net>
 
+use bootstrap::settings::BootstrapSettings;
+use consensus::ConsensusConfig;
+use models::api::APISettings;
+use models::Version;
+use network::NetworkSettings;
+use pool::PoolSettings;
+use protocol_exports::ProtocolSettings;
 use serde::Deserialize;
-use std::net::IpAddr;
-use std::sync::RwLock;
 
 const BASE_CONFIG_PATH: &str = "base_config/config.toml";
 const OVERRIDE_CONFIG_PATH: &str = "config/config.toml";
 
 lazy_static::lazy_static! {
-    static ref SETTINGS: RwLock<config::Config> = RwLock::new({
+    pub static ref SETTINGS: Settings = {
         let mut settings = config::Config::default();
-
         settings
             .merge(config::File::with_name(BASE_CONFIG_PATH))
             .unwrap();
-
         if std::path::Path::new(OVERRIDE_CONFIG_PATH).is_file() {
             settings
                 .merge(config::File::with_name(OVERRIDE_CONFIG_PATH))
@@ -23,25 +26,23 @@ lazy_static::lazy_static! {
         settings
             .merge(config::Environment::with_prefix("MASSA_CLIENT"))
             .unwrap();
-        settings
-    });
+        settings.try_into().unwrap()
+    };
+}
+
+#[derive(Debug, Deserialize, Clone, Copy)]
+pub struct LoggingSettings {
+    pub level: usize,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
-    pub default_node: DefaultNode,
-    pub history: usize,
-}
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct DefaultNode {
-    pub ip: IpAddr,
-    pub private_port: u16,
-    pub public_port: u16,
-}
-
-impl Settings {
-    pub(crate) fn load() -> Settings {
-        SETTINGS.read().unwrap().clone().try_into().unwrap()
-    }
+    pub logging: LoggingSettings,
+    pub protocol: ProtocolSettings,
+    pub network: NetworkSettings,
+    pub consensus: ConsensusConfig,
+    pub api: APISettings,
+    pub bootstrap: BootstrapSettings,
+    pub pool: PoolSettings,
+    pub version: Version,
 }

SecorD0/massa-fixing

api/src/lib.rs
@@ -11,7 +11,7 @@ use jsonrpc_derive::rpc;
 use jsonrpc_http_server::{CloseHandle, ServerBuilder};
 use models::address::{AddressHashMap, AddressHashSet};
 use models::api::{
-    APIConfig, AddressInfo, BlockInfo, BlockSummary, EndorsementInfo, NodeStatus, OperationInfo,
+    APISettings, AddressInfo, BlockInfo, BlockSummary, EndorsementInfo, NodeStatus, OperationInfo,
     TimeInterval,
 };
 use models::clique::Clique;
@@ -35,9 +35,9 @@ mod public;
 pub struct Public {
     pub consensus_command_sender: ConsensusCommandSender,
     pub pool_command_sender: PoolCommandSender,
-    pub consensus_config: ConsensusConfig,
-    pub api_config: APIConfig,
-    pub network_config: NetworkConfig,
+    pub consensus_settings: &'static ConsensusConfig,
+    pub api_settings: &'static APISettings,
+    pub network_settings: &'static NetworkConfig,
     pub version: Version,
     pub network_command_sender: NetworkCommandSender,
     pub compensation_millis: i64,
@@ -47,8 +47,8 @@ pub struct Public {
 pub struct Private {
     pub consensus_command_sender: ConsensusCommandSender,
     pub network_command_sender: NetworkCommandSender,
-    pub consensus_config: ConsensusConfig,
-    pub api_config: APIConfig,
+    pub consensus_settings: &'static ConsensusConfig,
+    pub api_settings: &'static APISettings,
     pub stop_node_channel: mpsc::Sender<()>,
 }
 

api/src/public.rs
@@ -9,7 +9,7 @@ use futures::{stream::FuturesUnordered, StreamExt};
 use jsonrpc_core::BoxFuture;
 use models::address::{AddressHashMap, AddressHashSet};
 use models::api::{
-    APIConfig, AddressInfo, BlockInfo, BlockInfoContent, BlockSummary, EndorsementInfo,
+    APISettings, AddressInfo, BlockInfo, BlockInfoContent, BlockSummary, EndorsementInfo,
     IndexedSlot, NodeStatus, OperationInfo, TimeInterval,
 };
 use models::clique::Clique;
@@ -30,10 +30,10 @@ use time::UTime;
 impl API<Public> {
     pub fn new(
         consensus_command_sender: ConsensusCommandSender,
-        api_config: APIConfig,
-        consensus_config: ConsensusConfig,
+        api_settings: &'static APISettings,
+        consensus_settings: &'static ConsensusConfig,
         pool_command_sender: PoolCommandSender,
-        network_config: NetworkConfig,
+        network_settings: &'static NetworkConfig,
         version: Version,
         network_command_sender: NetworkCommandSender,
         compensation_millis: i64,
@@ -41,10 +41,10 @@ impl API<Public> {
     ) -> Self {
         API(Public {
             consensus_command_sender,
-            consensus_config,
-            api_config,
+            consensus_settings,
+            api_settings,
             pool_command_sender,
-            network_config,
+            network_settings,
             version,
             network_command_sender,
             compensation_millis,
@@ -92,19 +92,19 @@ impl Endpoints for API<Public> {
     fn get_status(&self) -> BoxFuture<Result<NodeStatus, ApiError>> {
         let consensus_command_sender = self.0.consensus_command_sender.clone();
         let network_command_sender = self.0.network_command_sender.clone();
-        let network_config = self.0.network_config.clone();
+        let network_config = self.0.network_settings.clone();
         let version = self.0.version;
-        let consensus_config = self.0.consensus_config.clone();
+        let consensus_settings = self.0.consensus_settings.clone();
         let compensation_millis = self.0.compensation_millis;
         let mut pool_command_sender = self.0.pool_command_sender.clone();
         let node_id = self.0.node_id;
-        let algo_config = consensus_config.to_algo_config();
+        let algo_config = consensus_settings.to_algo_config();
         let closure = async move || {
             let now = UTime::now(compensation_millis)?;
             let last_slot = get_latest_block_slot_at_timestamp(
-                consensus_config.thread_count,
-                consensus_config.t0,
-                consensus_config.genesis_timestamp,
+                consensus_settings.thread_count,
+                consensus_settings.t0,
+                consensus_settings.genesis_timestamp,
                 now,
             )?;
 
@@ -118,11 +118,11 @@ impl Endpoints for API<Public> {
                 node_id,
                 node_ip: network_config.routable_ip,
                 version,
-                genesis_timestamp: consensus_config.genesis_timestamp,
-                t0: consensus_config.t0,
-                delta_f0: consensus_config.delta_f0,
-                roll_price: consensus_config.roll_price,
-                thread_count: consensus_config.thread_count,
+                genesis_timestamp: consensus_settings.genesis_timestamp,
+                t0: consensus_settings.t0,
+                delta_f0: consensus_settings.delta_f0,
+                roll_price: consensus_settings.roll_price,
+                thread_count: consensus_settings.thread_count,
                 current_time: now,
                 connected_nodes: peers?
                     .peers
@@ -133,7 +133,7 @@ impl Endpoints for API<Public> {
                 last_slot,
                 next_slot: last_slot
                     .unwrap_or_else(|| Slot::new(0, 0))
-                    .get_next_slot(consensus_config.thread_count)?,
+                    .get_next_slot(consensus_settings.thread_count)?,
                 consensus_stats: consensus_stats?,
                 network_stats: network_stats?,
                 pool_stats: pool_stats?,
@@ -141,7 +141,7 @@ impl Endpoints for API<Public> {
                 algo_config,
                 current_cycle: last_slot
                     .unwrap_or_else(|| Slot::new(0, 0))
-                    .get_cycle(consensus_config.periods_per_cycle),
+                    .get_cycle(consensus_settings.periods_per_cycle),
             })
         };
         Box::pin(closure())
@@ -163,7 +163,7 @@ impl Endpoints for API<Public> {
         &self,
         ops: Vec<OperationId>,
     ) -> BoxFuture<Result<Vec<OperationInfo>, ApiError>> {
-        let api_cfg = self.0.api_config;
+        let api_cfg = self.0.api_settings;
         let consensus_command_sender = self.0.consensus_command_sender.clone();
         let mut pool_command_sender = self.0.pool_command_sender.clone();
         let closure = async move || {
@@ -277,13 +277,13 @@ impl Endpoints for API<Public> {
         time: TimeInterval,
     ) -> BoxFuture<Result<Vec<BlockSummary>, ApiError>> {
         let consensus_command_sender = self.0.consensus_command_sender.clone();
-        let consensus_config = self.0.consensus_config.clone();
+        let consensus_settings = self.0.consensus_settings.clone();
         let closure = async move || {
             // filter blocks from graph_export
             let (start_slot, end_slot) = time_range_to_slot_range(
-                consensus_config.thread_count,
-                consensus_config.t0,
-                consensus_config.genesis_timestamp,
+                consensus_settings.thread_count,
+                consensus_settings.t0,
+                consensus_settings.genesis_timestamp,
                 time.start,
                 time.end,
             )?;
@@ -330,8 +330,8 @@ impl Endpoints for API<Public> {
         addresses: Vec<Address>,
     ) -> BoxFuture<Result<Vec<AddressInfo>, ApiError>> {
         let cmd_sender = self.0.consensus_command_sender.clone();
-        let cfg = self.0.consensus_config.clone();
-        let api_cfg = self.0.api_config;
+        let cfg = self.0.consensus_settings.clone();
+        let api_cfg = self.0.api_settings;
         let pool_command_sender = self.0.pool_command_sender.clone();
         let compensation_millis = self.0.compensation_millis;
         let closure = async move || {
@@ -446,7 +446,7 @@ impl Endpoints for API<Public> {
         ops: Vec<Operation>,
     ) -> BoxFuture<Result<Vec<OperationId>, ApiError>> {
         let mut cmd_sender = self.0.pool_command_sender.clone();
-        let api_cfg = self.0.api_config;
+        let api_cfg = self.0.api_settings;
         let closure = async move || {
             if ops.len() as u64 > api_cfg.max_arguments {
                 return Err(ApiError::TooManyArguments("too many arguments".into()));

consensus/src/consensus_controller.rs
@@ -1,12 +1,15 @@
 // Copyright (c) 2021 MASSA LABS <info@massa.net>
 
-use std::{collections::VecDeque, path::Path};
-
-use tokio::{
-    sync::{mpsc, oneshot},
-    task::JoinHandle,
+use super::{
+    block_graph::*,
+    config::{ConsensusConfig, CHANNEL_SIZE},
+    consensus_worker::{
+        ConsensusCommand, ConsensusEvent, ConsensusManagementCommand, ConsensusWorker,
+    },
+    pos::ProofOfStake,
 };
-
+use crate::error::ConsensusError;
+use crate::pos::ExportProofOfStake;
 use models::{
     address::{AddressHashMap, AddressHashSet, AddressState},
     BlockHashMap, OperationHashMap, OperationHashSet,
@@ -16,17 +19,10 @@ use models::{Address, Block, BlockId, OperationSearchResult, Slot, StakersCycleP
 use pool::PoolCommandSender;
 use protocol_exports::{ProtocolCommandSender, ProtocolEventReceiver};
 use signature::{derive_public_key, PrivateKey, PublicKey};
-
-use crate::error::ConsensusError;
-use crate::pos::ExportProofOfStake;
-
-use super::{
-    block_graph::*,
-    config::{ConsensusConfig, CHANNEL_SIZE},
-    consensus_worker::{
-        ConsensusCommand, ConsensusEvent, ConsensusManagementCommand, ConsensusWorker,
-    },
-    pos::ProofOfStake,
+use std::{collections::VecDeque, path::Path};
+use tokio::{
+    sync::{mpsc, oneshot},
+    task::JoinHandle,
 };
 use tracing::{debug, error, info};
 

massa-node/src/settings.rs
@@ -1,20 +1,23 @@
 // Copyright (c) 2021 MASSA LABS <info@massa.net>
 
+use bootstrap::settings::BootstrapSettings;
+use consensus::ConsensusConfig;
+use models::api::APISettings;
+use models::Version;
+use network::NetworkConfig;
+use pool::PoolSettings;
+use protocol_exports::ProtocolSettings;
 use serde::Deserialize;
-use std::net::IpAddr;
-use std::sync::RwLock;
 
 const BASE_CONFIG_PATH: &str = "base_config/config.toml";
 const OVERRIDE_CONFIG_PATH: &str = "config/config.toml";
 
 lazy_static::lazy_static! {
-    static ref SETTINGS: RwLock<config::Config> = RwLock::new({
+    pub static ref SETTINGS: Settings = {
         let mut settings = config::Config::default();
-
         settings
             .merge(config::File::with_name(BASE_CONFIG_PATH))
             .unwrap();
-
         if std::path::Path::new(OVERRIDE_CONFIG_PATH).is_file() {
             settings
                 .merge(config::File::with_name(OVERRIDE_CONFIG_PATH))
@@ -23,25 +26,23 @@ lazy_static::lazy_static! {
         settings
             .merge(config::Environment::with_prefix("MASSA_CLIENT"))
             .unwrap();
-        settings
-    });
+        settings.try_into().unwrap()
+    };
+}
+
+#[derive(Debug, Deserialize, Clone, Copy)]
+pub struct LoggingSettings {
+    pub level: usize,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
-    pub default_node: DefaultNode,
-    pub history: usize,
-}
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct DefaultNode {
-    pub ip: IpAddr,
-    pub private_port: u16,
-    pub public_port: u16,
-}
-
-impl Settings {
-    pub(crate) fn load() -> Settings {
-        SETTINGS.read().unwrap().clone().try_into().unwrap()
-    }
+    pub logging: LoggingSettings,
+    pub protocol: ProtocolSettings,
+    pub network: NetworkConfig,
+    pub consensus: ConsensusConfig,
+    pub api: APISettings,
+    pub bootstrap: BootstrapSettings,
+    pub pool: PoolSettings,
+    pub version: Version,
 }